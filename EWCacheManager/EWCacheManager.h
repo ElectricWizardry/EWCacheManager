620259a59bff69331f086c29bc2484c941a82ef1
Eric Bailey <eric@ericb.me>
2013-12-22 15:31:29 -0600

    initial commit

---
+++ EWCacheManager/EWCacheManager.h
@@ -0,0 +1,98 @@
+//
+//  EWCacheManager.h
+//
+//  Created by Eric Bailey on 12/6/13.
+//  Copyright (c) 2013 Electric Wizardry, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AFHTTPClient;
+@class AFHTTPRequestOperation;
+
+@class EWCacheManager;
+@protocol EWCacheManagerDelegate
+- (void)downloadFailedWithRequest:(NSURLRequest *)request filename:(NSString *)filename error:(NSError *)error;
+
+@optional
+- (void)downloadSucceededWithRequest:(NSURLRequest *)request filename:(NSString *)filename;
+@end
+
+/** The `EWCacheManager` class provides a singleton cache manager with convenience methods
+ * for downloading (via `AFNetworking`) and interacting with files that are store locally
+ * in `Library/Application Support/{BUNDLE_DISPLAY_NAME}`.
+ */
+@interface EWCacheManager : NSObject {
+	NSString *path;
+}
+
+@property (nonatomic, weak) id <EWCacheManagerDelegate> delegate;
+
+@property (nonatomic, strong) AFHTTPClient *downloadClient;
+@property (nonatomic, strong) NSURL *baseURL;
+
+@property (readonly) float fileProgress;
+@property (readonly) float queueProgress;
+@property (assign) NSUInteger requestedFilesCount;
+
+///---------------------------------------------
+/// @name Getting the Shared Cache Manager
+///---------------------------------------------
+
+/**
+ *  Returns the singleton cache manager.
+ */
++ (id)sharedManager;
+
+
+///---------------------------------------------
+/// @name Getting Information About Cached Files
+///---------------------------------------------
+
+/**
+ *  Returns the full path for the cached file with the specified filename.
+ *
+ *  @param filename The name of a cached file.
+ */
+- (NSString *)pathForFilename:(NSString *)filename;
+
+/**
+ *  Returns a `BOOL` representing a file's presence in the cache.
+ *
+ *  @param filename The name of a possibly cached file.
+ *
+ *  @return `YES` if the file is present, otherwise `NO`.
+ */
+- (BOOL)hasFileInCache:(NSString *)filename;
+
+/**
+ *  Returns the size in bytes of a file in the cache, or 0 if the file is not found.
+ *
+ *  @param filename The name of a cached file.
+ */
+- (unsigned long long)sizeOfFileInCache:(NSString *)filename;
+
+/**
+ *  Checks to see if a file exists in the cache, and if so, deletes it.
+ *
+ *  @param filename The name of a cached file.
+ */
+- (void)deleteFileInCache:(NSString *)filename;
+
+/**
+ *  Description
+ *
+ *  @param request  request description
+ *  @param filename filename description
+ *  @param success  success description
+ */
+- (void)downloadFileWithRequest:(NSURLRequest *)request filename:(NSString *)filename success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success;
+
+/**
+ *  Description
+ *
+ *  @return return value description
+ */
+- (NSString *)nameOfFileCurrentlyDownloading;
+
+@end
---
+++ EWCacheManagerTests/EWCacheManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  EWCacheManagerTests.m
+//  EWCacheManagerTests
+//
+//  Created by Eric on 12/18/13.
+//  Copyright (c) 2013 Electric Wizardry, LLc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EWCacheManagerTests : XCTestCase
+
+@end
+
+@implementation EWCacheManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
